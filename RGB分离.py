79572c327b3dcac6f9dc0e33dce6e03ad513116c
FangJoker <598592454@qq.com>
2018-02-11 17:30:44 +0800

    直方图计算

---
+++ RGB分离.py
@@ -0,0 +1,55 @@
+from PIL import Image
+import cv2  
+import numpy as np 
+
+
+
+#计算每个通道直方图
+#图像直方图是用以表示数字图像中亮度分布的直方图，标绘了图像中每个亮度值的像素数，可以借助观察该直方图了解需要如何调整亮度分布。
+def calcAndDrawHist(image, color):
+    #hist = cv2.calcHist([image],  
+    # img必须用[]括起来
+    # [0], #使用的通道  
+    # None, #没有使用mask  
+    #[256], #HistSize  表示这个直方图分成多少份（即多少个直方柱）
+    #[0.0,255.0]) #直方图柱的范围      
+    hist= cv2.calcHist([image], [0], None, [300], [0.0,255.0])    
+    #minMaxLoc寻找矩阵(一维数组当作向量,用Mat定义) 中最小值和最大值的位置
+    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(hist)    #结果为 (0.0, 17288.0, (0, 0), (0, 1))
+    #np.zeros();返回来一个给定形状和类型的用0填充的数组； eg: np.zeros(5)->array([ 0.,  0.,  0.,  0.,  0.])
+    #zeros(shape, dtype=float, order='C') shape:形状  dtype:数据类型，可选参数，默认numpy.float64   order:可选参数，c代表与c语言类似，行优先；F代表列优先
+    histImg = np.zeros([300,300,3], np.uint8)  #300 个 300行 3列
+    hpt = int(0.9* 300)
+       
+    
+    for h in range(300):  
+        #计算直方图的每个点的值 
+        intensity = int(hist[h]*hpt/maxVal)
+        #绘制直线  
+        cv2.line(histImg,(h,300), (h,300-intensity), color) 
+        #void line(Mat& img, Point pt1, Point pt2, const Scalar& color, int thickness=1, int lineType=8, int shift=0)
+        #img: 要绘制线段的图像。
+		#pt1: 线段的起点。
+		#pt2: 线段的终点。
+		#color: 线段的颜色，通过一个Scalar对象定义。
+		#hickness: 线条的宽度。
+		#lineType: 线段的类型。可以取值8， 4， 和CV_AA， 分别代表8邻接连接线，4邻接连接线和反锯齿连接线。默认值为8邻接。为了获得更好地效果可以选用CV_AA(采用了高斯滤波)。
+		#shift: 坐标点小数点位数。   
+    return histImg; 
+
+if __name__ == '__main__':    
+    img = cv2.imread("D:/pip/xiangjiao/1517974165.9889214.jpg")    
+    b, g, r = cv2.split(img)    
+    
+    histImgB = calcAndDrawHist(b, [255, 0, 0])    
+    histImgG = calcAndDrawHist(g, [0, 255, 0])    
+    histImgR = calcAndDrawHist(r, [0, 0, 255])    
+        
+    cv2.imshow("histImgB", histImgB)    
+    cv2.imshow("histImgG", histImgG)    
+    cv2.imshow("histImgR", histImgR)    
+    cv2.imshow("Img", img)    
+    #waitKey()函数的功能是不断刷新图像，频率时间为delay，单位为ms。
+    cv2.waitKey(0)    
+    #关闭所有的HighGUI窗口
+    cv2.destroyAllWindows()   
---
+++ RGB合并.py
@@ -0,0 +1,38 @@
+import cv2 as cv2
+import numpy as np    
+         
+img = cv2.imread('D:/pip/xiangjiao/1517974165.9889214.jpg')    
+h = np.zeros((300,300,3),np.uint8)  #作为折线输入图  全0图像    
+         
+bins = np.arange(300).reshape(300,1) #直方图中各bin的顶点位置   (生成一个256行1列的数组) 
+color = [ (299,0,0),(0,299,0),(0,0,299) ] #BGR三种颜色    
+#对三个通道都遍历一遍（枚举3个通道）
+
+for ch, col in enumerate(color):    
+    #计算ch通道直方图
+    originHist = cv2.calcHist([img],[ch],None,[300],[0,300]) 
+    #Opencv2的归一化函数normalize()，使得直方图的范围限定在0-255×0.9之间
+    #void normalize(InputArray src,OutputArray dst, double alpha=1, doublebeta=0, int norm_type=NORM_L2, int dtype=-1, InputArray mask=noArray() )
+    #src  输入数组   dst 输出数组，支持原地运算
+    #归一类型：
+    #NORM_MINMAX:数组的数值被平移或缩放到一个指定的范围，线性归一化，一般较常用。
+    #NORM_INF: 此类型的定义没有查到，根据Opencv2 1的对应项，可能是归一化数组的C-范数(绝对值的最大值)
+    #NORM_L1 :  归一化数组的L1-范数(绝对值的和)
+    #NORM_L2: 归一化数组的(欧几里德)L2-范数
+    cv2.normalize(originHist, originHist,0,299*0.9,cv2.NORM_MINMAX)
+    #使用around对矩阵中每一个元素取整（四舍五入）
+    #因为calcHist函数返回的是float32类型的数组所以将整数部分转成np.int32类型。 例如66.666->66.0->66
+    #注意：Python的int(...)只能转换一个元素。这里是很多个元素，numpy的转换函数可以对数组中的每个元素都进行转换
+    hist=np.int32(np.around(originHist)) 
+    #column_stack()取一组一维数组并将它们堆叠成一列以构成一个二维数组。
+    #将binds与hist转成相应的坐标，然后一一对应
+    pts = np.column_stack((bins,hist))
+    #绘制出折线 
+    #第三个False参数指出不是封闭图形
+    #第四个参数指定了折线的颜色。 3个通道3个颜色
+    cv2.polylines(h,[pts],False,col)
+    
+#反转绘制好的直方图，因为绘制时，[0,0]在会图像的左上角。
+h = np.flipud(h)
+cv2.imshow('img',h)  
+cv2.waitKey(0)  
